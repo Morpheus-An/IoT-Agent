d9dc9f395f107e4074ec19effa0303daab67cac9
antuo <211300040@smail.nju.edu.cn>
2025-03-02 20:27:19 +0800

    new baseline for IMU-3clsgit add common/generate_prompt.py  after fixing that buggit add common/generate_prompt.py

--- common/generate_prompt.py
+++ common/generate_prompt.py
@@ -136,27 +136,22 @@ def gen_prompt_template_with_rag_imu(args, label2ids, data_dict, ground_ans: str
         gyr_x = data_dict[label2ids[target_cls]]["body_gyro"][i][0]
         gyr_y = data_dict[label2ids[target_cls]]["body_gyro"][i][1]
         gyr_z = data_dict[label2ids[target_cls]]["body_gyro"][i][2] 
-        acc_x_str = ", ".join([f"{format_number(x)} g" for x in acc_x])
-        acc_y_str = ", ".join([f"{format_number(x)} g" for x in acc_y])
-        acc_z_str = ", ".join([f"{format_number(x)} g" for x in acc_z])
-        gyr_x_str = ", ".join([f"{format_number(x)} rad/s" for x in gyr_x])
-        gyr_y_str = ", ".join([f"{format_number(x)} rad/s" for x in gyr_y])
-        gyr_z_str = ", ".join([f"{format_number(x)} rad/s" for x in gyr_z])
+        acc_x_str = ", ".join([f"{x}" for x in acc_x])
+        acc_y_str = ", ".join([f"{x}" for x in acc_y])
+        acc_z_str = ", ".join([f"{x}" for x in acc_z])
+        gyr_x_str = ", ".join([f"{x}" for x in gyr_x])
+        gyr_y_str = ", ".join([f"{x}" for x in gyr_y])
+        gyr_z_str = ", ".join([f"{x}" for x in gyr_z])
         data_des = f"""
 1. Triaxial acceleration signal: 
 X-axis: {acc_x_str} 
 Y-axis: {acc_y_str} 
 Z-axis: {acc_z_str} 
-X-axis-mean={np.around(np.mean(acc_x), 3)}g, X-axis-var={np.around(np.var(acc_x), 3)} 
-Y-axis-mean={np.around(np.mean(acc_y), 3)}g, Y-axis-var={np.around(np.var(acc_y), 3)} 
-Z-axis-mean={np.around(np.mean(acc_z), 3)}g, Z-axis-var={np.around(np.var(acc_z), 3)} 
 2. Triaxial angular velocity signal: 
 X-axis: {gyr_x_str} 
 Y-axis: {gyr_y_str} 
 Z-axis: {gyr_z_str}
-X-axis-mean={np.around(np.mean(gyr_x), 3)}rad/s, X-axis-var={np.around(np.var(gyr_x), 3)} 
-Y-axis-mean={np.around(np.mean(gyr_y), 3)}rad/s, Y-axis-var={np.around(np.var(gyr_y), 3)} 
-Z-axis-mean={np.around(np.mean(gyr_z), 3)}rad/s, Z-axis-var={np.around(np.var(gyr_z), 3)}"""
+"""
         return data_des
     if args.cls_num == 2:
         data_des = create_data_des(i)
